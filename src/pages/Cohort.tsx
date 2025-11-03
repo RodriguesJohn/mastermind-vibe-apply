934736d48c97d8fe5ab9051ff27183fa22401d7d
John Rodrigues <johnrodrigues@Johns-MacBook-Pro.local>
2025-11-02 21:44:02 -0800

    Transform site to Design Engineer course: update header, add outcome cards, update curriculum (HTML/CSS/JS, React, Tailwind, Next.js, Portfolio)

--- src/pages/Cohort.tsx
+++ src/pages/Cohort.tsx
@@ -1,6 +1,7 @@
 import { Navigation } from "@/components/Navigation";
 import { AIHeaderSection } from "@/components/AIHeaderSection";
 import { CohortOverviewSection } from "@/components/CohortOverviewSection";
+import { CohortOutcomesSection } from "@/components/CohortOutcomesSection";
 import { CohortModulesSection } from "@/components/CohortModulesSection";
 import { CohortTestimonialsSection } from "@/components/CohortTestimonialsSection";
 import { InstructorSection } from "@/components/InstructorSection";
@@ -12,8 +13,8 @@ const Cohort = () => {
   return (
     <>
       <Helmet>
-        <title>AI Mastery for Designers & Product Leaders 5-Week Cohort</title>
-        <meta name="description" content="Join our coding cohort and learn with a community of developers." />
+        <title>Become a Design Engineer - Design Engineering Course</title>
+        <meta name="description" content="Master HTML, CSS, JavaScript, React, Tailwind, and Next.js to become a design engineer. Build your portfolio and accelerate your career." />
       </Helmet>
 
       <div className="min-h-screen">
@@ -21,7 +22,7 @@ const Cohort = () => {
         <main>
           <AIHeaderSection />
           <CohortOverviewSection />
-          <CohortTestimonialsSection />
+          <CohortOutcomesSection />
           <CohortModulesSection />
           <InstructorSection />
           <PlaybookSection />
